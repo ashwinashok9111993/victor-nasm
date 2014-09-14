017da554bdb4de3713d5b3495b9a5feabb8a6aee
ashwin <ashwinashok9111993@gmail.com>
2014-09-14 13:51:42 +0530

    Update arraydemo.s

--- arraydemo.s
+++ arraydemo.s
@@ -1,5 +1,7 @@
-.section .data
+section .data
 values: .int 10,20,30,40,50,60,80,90,100
+values1: .int 11,22,33,44,55,66,77,88,110
+
 str:    .asciz "%d\t"
 .section .bss
 
@@ -12,9 +14,8 @@ main:
         movl  $values, %edi
         movl  $9,%ecx
 l1:     nop
-        movl (%edi),%ebx
         pushl %ecx
-        pushl %ebx
+        pushl (%edi)
         pushl $str
         call  printf
         addl  $8,%esp
