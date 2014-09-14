fda3d9c40fea00beb49d97672e243e5f17ab9215
ashwin <ashwinashok9111993@gmail.com>
2014-09-14 14:22:31 +0530

    Update printdemo.s

--- printdemo.s
+++ printdemo.s
@@ -6,7 +6,7 @@ hello: .asciz "hello world!\n"
 main:
         pushl $hello
         call printf
-        addl $8,%esp
+        addl $4,%esp
         movl $1,%eax
         movl $0,%ebx
         int  $0x80
